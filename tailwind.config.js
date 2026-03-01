0fae56e554b016bb2615f25aa38e3849f5380971
kanishkhaa <151703688+kanishkhaa@users.noreply.github.com>
2026-03-01 13:59:38 +0530

    Create Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
